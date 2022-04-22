ed6041ff295a49ab37244cfec4be1dcbd1d7d9bf
Maksim-git <dv.maksim11@gmail.com>
2022-04-22 08:44:31 +0300

    improved code for clearing data from the field after element creation

--- script.js
+++ script.js
@@ -1,33 +1,26 @@
-const addButton = document.querySelector('.form__button');
-const formInput = document.querySelector('.form__input');
-const list = document.querySelector('.list');
+const addButton = document.querySelector(".form__button");
+const formInput = document.querySelector(".form__input");
+const list = document.querySelector(".list");
 
-addButton.addEventListener('click', ()=>{
+addButton.addEventListener("click", () => {
   let fieldValue = formInput.value;
-  const createList = document.createElement('li');
-  createList.classList.add('list__li');
+  const createList = document.createElement("li");
+  createList.classList.add("list__li");
   if (fieldValue) {
     createList.innerHTML = `${fieldValue} <button class="delete-button">Delete</button>`;
-    } else {
-    const addingEmptyValue = 'The field is empty';
+  } else {
+    const addingEmptyValue = "The field is empty";
     createList.innerHTML = `${addingEmptyValue} <button class="delete-button">Delete</button>`;
-    }
-    list.prepend(createList);
-    ClearFields()
-})
+  }
+  list.prepend(createList);
+  formInput.value = "";
+});
 
-function ClearFields() {
-    document.querySelector('.form__input').value = "";
-}
-
-list.addEventListener('click',(event)=>{
+list.addEventListener("click", (event) => {
   const item = event.target;
-   if (item.classList.contains('delete-button')){
-    item.closest('li').remove();
-    }
-     else if (item.tagName === 'LI'){
-      item.classList.toggle('active');
-    }
-})
-
-
+  if (item.classList.contains("delete-button")) {
+    item.closest("li").remove();
+  } else if (item.tagName === "LI") {
+    item.classList.toggle("active");
+  }
+});
